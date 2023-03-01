56e5d7ff531440d7d206184c7a09c78432d874c3
Almas Mirzandi <almasmirzandir@gmail.com>
2023-03-01 12:55:43 +0700

    updated google auth_service

--- puree_app/lib/services/auth_service.dart
+++ puree_app/lib/services/auth_service.dart
@@ -1,21 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
-  //Google Sign in
-  signInWithGoogle() async {
-    //begin interactive sign in process
-    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-    //obtain auth details from request
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+  Future<bool> signInWithGoogle() async {
+    bool result = false;
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
 
-    //create new credentials
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken,
-    );
-    //finally sign in process
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      User? user = userCredential.user;
+
+      if (user != null) {
+        if (userCredential.additionalUserInfo!.isNewUser) {
+          //add additional user to firestore
+          await _firestore.collection('merchant').doc(user.uid).set({
+            'usernames': user.displayName,
+            'uid': user.uid,
+            'profilePhoto': user.photoURL,
+            'email': user.email
+          });
+        }
+        result = true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
   }
 }
